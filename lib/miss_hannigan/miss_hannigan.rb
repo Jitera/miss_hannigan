7e2b411b17c7bd4f661c1c11387b368ec788508b
eebasadre20 <eebasadre20@gmail.com>
2022-09-24 11:49:41 +0800

    prevent checking if the table has changed the foreign key null on migration

--- lib/miss_hannigan/miss_hannigan.rb
+++ lib/miss_hannigan/miss_hannigan.rb
@@ -35,10 +35,11 @@ module MissHannigan
 
       # I bet folks are going to forget to do the migration of foreign_keys to accept null. Rails defaults
       # to not allow null.
-      if !reflection_details.klass.columns.find { |c| c.name == reflection_details.foreign_key }.null
-        raise "The foreign key must be nullable to support MissHannigan. You should create a migration to:
-          change_column_null :#{name.to_s}, :#{reflection_details.foreign_key}, true"
-      end
+      # NOTE: We will just make sure that it has a migration to change column null
+      # if !reflection_details.klass.columns.find { |c| c.name == reflection_details.foreign_key }.null
+      #   raise "The foreign key must be nullable to support MissHannigan. You should create a migration to:
+      #     change_column_null :#{name.to_s}, :#{reflection_details.foreign_key}, true"
+      # end
 
       after_destroy do |this_object|
         CleanupJob.perform_later(reflection_details.klass.to_s, reflection_details.foreign_key)
